ffb7679ea6c24d909a57bb17ccd73c6f9f6a69e2
Guebes <150551019+Guebes@users.noreply.github.com>
2025-08-18 09:22:39 -0300

    Update main.js
    Atualizei a parte do Jquery da mask, input telefone

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
 $(document).ready(function () {
 
-    $('#input-telefone').mask('(00) 00000-0000', {
-        placeholder: '(00) 00000-0000'
-    })
-
-    $('#id-formulario').validate({
-
-
-        rules:{
-
-
-            telefone:{
-
-                required: true,
-                minlength: 14  
-            }
-        }
-    })
-
+    $('#input-telefone').mask('(00) 00000-0000')
 })
