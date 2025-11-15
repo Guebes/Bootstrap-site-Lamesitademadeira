59a32b79afc9fdd40d57eb72bb0ed7804e2430a7
Guebes <150551019+Guebes@users.noreply.github.com>
2025-11-15 11:58:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,40 @@
 $(document).ready(function () {
 
-    $('#input-telefone').mask('(00) 00000-0000')
-})
+    $('#input-telefone').mask('(00) 00000-0000');
+
+    $('#id-formulario').validate({
+
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mensagem: {
+                required: true
+            }
+        },
+
+        messages: {
+            nome: 'Forneça o seu nome',
+            email: {
+                required: 'Forneça o seu e-mail',
+                email: 'Digite um e-mail válido'
+            },
+            mensagem: 'Escreva sua mensagem'
+        },
+
+        submitHandler: function (form) {
+            alert('Formulário enviado com sucesso!');
+
+            // Limpar os campos
+            $('#input-nome').val('');
+            $('#input-email').val('');
+            $('#input-telefone').val('');
+            $('#input-mensagem').val('');
+        },
+
+    });
+});
